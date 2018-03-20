a58abf841c9811b14b9243ae66171487ffb0de2c
Kirsch Marc <KirschMarc@hotmail.com>
2018-03-20 19:28:02 +0100

    Init project

---
+++ src/main/java/lu/ryo/skylibrary/SkylibraryApplication.java
@@ -0,0 +1,12 @@
+package lu.ryo.skylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylibraryApplication.class, args);
+	}
+}
